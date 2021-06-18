25b70d099139cb80201a574380bb225e0982d93b
zejia.lu <zejia.lu@ucloud.cn>
2021-06-18 15:58:39 +0800

    add es and kafka monitor and kafka top

--- .history/cmd/main_20210618095504.go
+++ .history/cmd/main_20210618095504.go
@@ -6,9 +6,11 @@ import (
 )
 
 func setup(kafkaAddr *map[string][]string) {
-	(*kafkaAddr)["my-kafka-cluster1"] = []string{"my-kafka-headless.default.svc.cluster.local:9092"}
-	(*kafkaAddr)["my-kafka-cluster2"] = []string{"ipv4:9092"}
-	(*kafkaAddr)["my-kafka-cluster3"] = []string{"[ipv6]:9092"}
+	//(*kafkaAddr)["my-kafka"] = []string{"my-kafka-headless.default.svc.cluster.local:9092"}
+	//(*kafkaAddr)["my-kafka"] = []string{"10.7.23.139:9092"}
+	//(*kafkaAddr)["my-kafka-c1"] = []string{"[2002:ac12:dc05::fe:c13f:2c0b]:9092"}
+	//(*kafkaAddr)["my-kafka-c2"] = []string{"[2002:a43:b808::fe:8f72:d173]:9092"}
+	//(*kafkaAddr)["my-kafka-c3"] = []string{"[2002:a42:b276::fe:b632:e00d]:9092"}
 }
 
 func main() {
